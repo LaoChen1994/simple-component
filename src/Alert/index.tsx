a3c06b1725e0cc96416d0289d804342583c3d133
cyx <735849467@qq.com>
2022-04-17 16:58:14 +0800

    feat: init

---
+++ src/Alert/index.tsx
@@ -0,0 +1,34 @@
+import type { PropsWithChildren } from 'react';
+import React from 'react';
+
+export interface AlertProps {
+  type?: 'info' | 'positive' | 'negative' | 'warning';
+}
+
+export type KindMap = Record<Required<AlertProps>['type'], string>;
+
+const prefixCls = 'happy-alert';
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert = (props: PropsWithChildren<AlertProps>) => {
+  const { type = 'info', children, ...rest } = props;
+  return (
+    <div
+      className={prefixCls}
+      style={{
+        background: kinds[type],
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Alert;
